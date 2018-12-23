9a8740f2e65a359a6614f180905541715c90c415
canqel <canqel@gmail.com>
2018-12-23 21:50:21 +0100

    feat(deck-editor): added deck header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'fdm-root',
   template: `
-    <div style="text-align:center">
-      <h1>
-        Flash Decks Mate
-      </h1>
+    <div class="mat-display-1" style="text-align:center; margin:2rem 0 3rem 0;">
+      Flash Decks Mate
     </div>
-    <router-outlet></router-outlet>
+    <div class="page-container">
+      <router-outlet></router-outlet>
+    </div>
   `,
   styles: []
 })
--- src/app/decks/decks.module.ts
+++ src/app/decks/decks.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DeckEditorComponent } from './pages/deck-editor/deck-editor.component';
 import { DecksRoutingModule } from './decks-routing.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [DeckEditorComponent],
   imports: [
     CommonModule,
-    DecksRoutingModule
+    DecksRoutingModule,
+    MatButtonModule,
+    MatIconModule
   ],
   entryComponents: [DeckEditorComponent]
 })
